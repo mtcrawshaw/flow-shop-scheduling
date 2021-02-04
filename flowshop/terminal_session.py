881775156166eb0d510ac5a5a93be5ce4cc2ed1f
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2021-02-03 19:44:53 -0500

    Added dummy display with curses.

--- flowshop/terminal_session.py
+++ flowshop/terminal_session.py
@@ -1,5 +1,9 @@
 """ Interface definition for running a Session from the terminal. """
 
+from time import sleep
+import curses
+from curses import wrapper
+
 from flowshop.session import Session
 from flowshop.utils import Direction
 
@@ -12,12 +16,41 @@ class TerminalSession:
         self.session = Session(name, load=load)
 
     def run(self) -> None:
-        """ Runs the session. Only needs to be called once at the beginning. """
-        pass
+        """ Exposed wrapper around `self._run()` that uses curses wrapper. """
+        wrapper(self._run)
 
-    def display(self) -> None:
-        """ Displays the session. Called any time a user action is taken. """
-        pass
+    def _run(self, stdscr) -> None:
+        """ Runs the session. Only needs to be called once at beginning of session. """
+
+        # Clear screen.
+        stdscr.clear()
+
+        # Display the current session and wait for a key press.
+        self.display(stdscr)
+
+        # Wait for 5 seconds to see screen.
+        sleep(5)
+
+    def display(self, stdscr) -> None:
+        """ Displays the session. Called any time the screen must be refreshed. """
+
+        # Draw some test pads.
+        x = (curses.COLS - 1) // 6
+        y = (curses.LINES - 1) // 6
+        x1 = x
+        y1 = y
+        x2 = 2 * x
+        y2 = 2 * y
+        x3 = 4 * x
+        y3 = 4 * y
+        x4 = 5 * x
+        y4 = 5 * y
+        p1 = curses.newpad(y2 - y1, x2 - x1)
+        p2 = curses.newpad(y4 - y3, x4 - x3)
+        p1.box()
+        p2.box()
+        p1.refresh(0, 0, y1, x1, y2, x2)
+        p2.refresh(0, 0, y3, x3, y4, x4)
 
     def navigate(self, direction: Direction, big=False) -> None:
         """
